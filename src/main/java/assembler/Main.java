c2209202e0ea4481405033c7906d80cea367a141
elana <ElanaL1@gmail.com>
2019-04-24 16:44:10 -0400

    create basic assembler

--- src/main/java/assembler/Main.java
+++ src/main/java/assembler/Main.java
@@ -6,13 +6,7 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-//        assumes machine language code is correct
-        // need to allow for filepath to be user input later...
         String filePath = args[0];
-//        String filePath = "/Users/elana/Documents/nand2tetris/projects/06/max/Max.asm";
-//        String filePath = "/Users/elana/Documents/nand2tetris/projects/06/pong/Pong.asm";
-//        String filePath = "/Users/elana/Documents/nand2tetris/projects/06/rect/Rect.asm";
-//        String filePath = "/Users/elana/Documents/nand2tetris/projects/06/file.asm";
         FileHandler fileHandler = new FileHandler(filePath);
         fileHandler.handle();
     }
